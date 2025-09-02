ad1bf478fe2c35acd5c64fecf310879dd15a5ba9
Robert Wiebe <robert.wiebe9@icloud.com>
2025-09-02 11:13:30 +0200

    add visualization examples

---
+++ examples.py
@@ -0,0 +1,17 @@
+# Some cool sample visualizations made using the library
+from manifold import *
+import numpy as np
+import torch
+
+def torus_geodesic_example():
+    torus = Torus()
+    torus.show_source_geodesic([-np.pi/4, 0.0], [-0.3, 1.0], length=10.0, show_initial_vector=True)
+
+def mobius_translation_example():
+    strip = MobiusStrip()
+    strip.show_parallel_transport(coord_lerp([0.0, 0.0], [2*np.pi, 0.0]), [0.0, 0.4], n_substeps=12)
+
+
+if __name__ == "__main__":
+    # torus_geodesic_example()
+    mobius_translation_example()
